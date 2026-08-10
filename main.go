4a5235c555eef66f7734c3dfcbf56c218d844ac6
Evgeniy Kucher <evgen-kucher@ukr.net>
2026-08-10 20:58:00 +0300

    Validate TELE_TOKEN in root entrypoint

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/ekucher/kbot/cmd"
 )
 
 func main() {
+	if os.Getenv("TELE_TOKEN") == "" {
+		log.Fatal("environment variable TELE_TOKEN is not set")
+	}
+
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
